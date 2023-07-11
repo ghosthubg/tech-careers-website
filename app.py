99a43e0cdee8b3aa67e34aa351737143d8b1f8da
ghosthubg <91081294+ghosthubg@users.noreply.github.com>
2023-07-11 19:55:30 +0000

    added the flask project

---
+++ app.py
@@ -0,0 +1,17 @@
+#import flask
+from flask import  Flask
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello world</p>"
+
+
+
+#check if running as python script
+#start app locally
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
